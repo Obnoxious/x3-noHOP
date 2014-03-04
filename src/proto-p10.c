8f7d8cd01227e3ae284619bc5d5bd6d3caed9dbc
Obnoxious <obnoxious@binhex.org>
2014-03-04 18:14:02 +0000

    Possible fix for SHUNs not working. We'll see once I try it out.

--- src/proto-p10.c
+++ src/proto-p10.c
@@ -837,7 +837,7 @@ irc_gline(struct server *srv, struct gline *gline, int silent)
 void
 irc_shun(struct server *srv, struct shun *shun)
 {
-    putsock("%s " P10_SHUN " %s +%s " FMT_TIME_T " :<%s> %s",
+    putsock("%s " P10_SHUN " %s +%s " FMT_TIME_T " " FMT_TIME_T " :%s<%s> %s",
             self->numeric, (srv ? srv->numeric : "*"), shun->target, shun->expires-now, shun->issuer, shun->reason);
 }
 
